3edef4f74543c7dec597f450766207c8c211bb2b
saitejatamiri <98472448+saitejatamiri@users.noreply.github.com>
2025-08-21 16:21:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,28 +3,4 @@ output "cluster_name" {
   value       = google_container_cluster.primary.name
 }
 
-output "cluster_endpoint" {
-  description = "GKE cluster endpoint"
-  value       = google_container_cluster.primary.endpoint
-}
 
-output "cluster_ca_certificate" {
-  description = "GKE cluster CA certificate"
-  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  sensitive   = true
-}
-
-output "vpc_name" {
-  description = "VPC name"
-  value       = google_compute_network.gke_vpc.name
-}
-
-output "subnet_name" {
-  description = "Subnet name"
-  value       = google_compute_subnetwork.gke_subnet.name
-}
-
-output "node_pool_name" {
-  description = "Node pool name"
-  value       = google_container_node_pool.primary_nodes.name
-}
